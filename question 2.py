680a30eb0d05b59145b2b68af58a640b7b241724
RamgopalYalla <ramgopal678@gmail.com>
2020-12-08 20:46:09 +0100

    question 2

--- question 2.py
+++ question 2.py
@@ -19,6 +19,6 @@ if num < 0:
 elif num == 0:
     print("The factorial of 0 is 1")
 else:
-    for i in range(1, num + 1):
+    for i in range(0, num + 1):
         factorial = factorial*i
     print(factorial)
