fc6984d2993be47b5e6cc7297d86029692220868
RamgopalYalla <ramgopal678@gmail.com>
2020-12-08 20:58:49 +0100

    question 3

--- question 3.py
+++ question 3.py
@@ -1,5 +1,13 @@
-#With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
-#Suppose the following input is supplied to the program:
-#8
-#Then, the output should be:
-#{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+# With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+x = int(input("Enter a number: "))
+if x == 0:
+    print("Enter a nonzero number")
+else:
+    for i in range(1, x):
+        res = {i: (i * i)}
+        print(res)
