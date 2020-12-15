62d904fe58d7ee854662d3afa312fd9f8ad84eee
RamgopalYalla <ramgopal678@gmail.com>
2020-12-15 21:37:48 +0100

    question 5

--- question 6.py
+++ question 6.py
@@ -17,11 +17,12 @@ In case of input data being supplied to the question, it should be assumed to be
 """
 
 import math
-c=50
-h=30
-value = []
-items=[x for x in raw_input().split(',')]
-for d in items:
-    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
 
-print ','.join(value)
+c = 50
+h = 30
+value = []
+items = [x for x in input().split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2 * c * float(d) / h)))))
+
+print(','.join(value))
