5f7c1f5e07ebf3c41f8cdae9f7fb4eaaf19561bb
[RAM] <[ramgopal678@gmail.com]>
2021-01-24 21:15:30 +0100

    challenge 1

---
+++ challenge 1.py
@@ -0,0 +1,11 @@
+"""
+QUESTION.
+         Write a function that stutters a word as if someone is struggling to read it.
+         The first two letters are repeated twice with an ellipsis ... and space after each, and
+         then the word is pronounced with a question mark ?.
+
+Example:
+        stutter("incredible") ➞ "in... in... incredible?"
+
+        stutter("enthusiastic") ➞ "en... en... enthusiastic?"
+"""
